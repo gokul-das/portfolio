db845ea244d037afea359c65499c136ece9afce6
unknown <dgokul257@gmail.com>
2022-09-16 22:29:30 +0530

    start

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click' , () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction)) 
+
+  
+/*==================== ACCORDION SKILLS ====================
+const skillsContent = document.getElementsByClassName('skills__content')
+        skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].classsName = 'skills__content skills__close'
+    }
+    if(itemClass ==='skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    e1.addEventListener('click', toggleSkills)
+})
+
+==================== QUALIFICATION TABS ====================
+const tabs = document.querySelectorAll('[data-target'),
+        tabContents = document.querySelectorAll('[data-content')
+
+        tabs.forEach(tab =>{
+            tab.addEventListener('click', () =>{
+                const tar
+            }
+
+        })*/
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
